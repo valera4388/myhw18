08bf25a957e4b056c49ef16c955bf5b7e4e91bc8
mikhalev valera <mihalev.valera@mail.ru>
2024-11-02 00:06:15 +0300

    CI crush test

--- src/test/java/ru/netology/rest/MobileBankApiTestV6.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV6.java
@@ -29,6 +29,6 @@ class MobileBankApiTestV6 {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(200);
+                .statusCode(400);
     }
 }
